5f0744faf7cfe2a10f02c604544785ad6418f0b1
tfai0249 <tfai0249@uni.sydney.edu.au>
2025-10-04 19:24:21 +1000

    changing hand position root idk just playing around

--- digestQuest/Assets/Scripts/HandManager.cs
+++ digestQuest/Assets/Scripts/HandManager.cs
@@ -7,15 +7,27 @@ namespace DigestQuest
     {
         public GameObject cardPrefab;
         public Transform handTransform;
-        public float fanSpread = 2f;
+
+        public float cardSpacing = 375; // from trial and error on the game
 
         public List<GameObject> cardsInHand = new List<GameObject>();
 
         void Start()
         {
+
+            handTransform.position = handTransform.position + new Vector3(-100f, -250f, 0f); // moves hand down by 50 units
+
             AddCardToHand();
             AddCardToHand();
             AddCardToHand();
+            AddCardToHand();
+            AddCardToHand();
+        }
+
+        void Update()
+        {
+
+            UpdateHandVisuals();
         }
 
         public void AddCardToHand()
@@ -30,12 +42,12 @@ namespace DigestQuest
             int cardCount = cardsInHand.Count;
             if (cardCount == 0) return;
 
-            float totalWidth = (cardCount - 1) * fanSpread;
+            float totalWidth = (cardCount - 1) * cardSpacing;
             Vector3 handCenter = handTransform.position;
 
             for (int i = 0; i < cardCount; i++)
             {
-                float xOffset = (i * fanSpread) - (totalWidth / 2f);
+                float xOffset = (i * cardSpacing) - (totalWidth / 2f);
                 Vector3 cardPos = handCenter + new Vector3(xOffset, 0, 0);
                 cardsInHand[i].transform.position = cardPos;
                 cardsInHand[i].transform.rotation = Quaternion.identity;
