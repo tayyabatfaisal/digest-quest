8915af29a1096bfbf495fceccd922e19f61c1368
tfai0249 <tfai0249@uni.sydney.edu.au>
2025-10-04 20:02:06 +1000

    can add pre made cards onto the hand. there is still no limit thought

--- digestQuest/Assets/Scripts/DeckManager.cs
+++ digestQuest/Assets/Scripts/DeckManager.cs
@@ -3,22 +3,28 @@ using System.Collections.Generic;
 
 namespace DigestQuest
 {
-    public class DeckManager
+    public class DeckManager : MonoBehaviour // <--- Must inherit MonoBehaviour!
+    //only monobehaviour scripts can be attatched to game objects 
     {
         public List<Card> allCards = new List<Card>();
         private int currentIndex = 0;
 
-        public void DrawCard(HandManager handManager) 
-         {
-            if (allCards.Count == 0)
-            {
-                return;
-            }
 
-            Card nextCard = allCards[currentIndex];
-            handManager.AddCardToHand(nextCard);
-            currentIndex = (currentIndex + 1) % allCards.Count; //wrap around to the beginning
+        void Start()
+        {
+            //load all card assets from the cardData resrouces 
+            Card[] cards = Resources.LoadAll<Card>("Cards");
+
+            //add the loaded cards to the allCards list
+            allCards.AddRange(cards);
         }
 
+        public void DrawCard(HandManager handManager)
+        {
+            if (allCards.Count == 0) return;
+            Card nextCard = allCards[currentIndex];
+            handManager.AddCardToHand(nextCard);
+            currentIndex = (currentIndex + 1) % allCards.Count;
+        }
     }
 }
